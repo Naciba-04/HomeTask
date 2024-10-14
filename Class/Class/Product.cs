a44dab02cfadcadc55f85635bd298be1243138b2
naciba-04 <nacibar-ab108@code.edu.az>
2024-10-15 00:56:06 +0400

    class

---
+++ Class/Class/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+     class Product
+    {
+        public string Name;
+        public string BrandName;
+        public double Price;
+        public Product(string name, string brandName, double price)
+        {
+            Name = name;
+            this.BrandName = brandName;
+            Price = price;
+        }
+    }
+}
---
+++ Class/Class/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product canta = new Product("Canta", "Michael Kors", 400);
+            Product ayaqqabi = new Product("Ayaqqabi", "Nike", 200);
+            Product parfum = new Product("Parfum", "Libre", 350);
+            Product masin = new Product("Masin", "Porsche", 93500);
+            Product[] products = new Product[] { canta, ayaqqabi, parfum, masin };
+            double minPrice = 100;
+            double maxPrice = 100000;
+            PrintProductsInRange(products, minPrice, maxPrice);
+        }
+        public static void PrintProductsInRange(Product[] products, double minPrice, double maxPrice)
+        {
+            Console.WriteLine($"Mehsullarin qiymet araligi: {minPrice} - {maxPrice}");
+            foreach (var product in products)
+            {
+                if (product.Price >= minPrice && product.Price <= maxPrice)
+                    Console.WriteLine($"{product.Name} - {product.BrandName}: {product.Price} AZN");
+            }
+        }
+    }
+}
